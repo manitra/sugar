4e884d633be478422b1f138e33b444dc1b922dbf
Manitra Andriamitondra <manitra@supermarche.mg>
2023-04-09 22:31:35 +0300

    Initial commit

---
+++ Sugar.Parallel/Program.cs
@@ -0,0 +1,19 @@
+﻿using Sugar.Parallel;
+
+using var runner = new ConcurrentRunner(20);
+
+for (int i = 0; i < 1000; i++)
+{
+    Console.WriteLine($"Main {i}");
+    var local = i;
+    await runner.Run(async () =>
+    {
+        Console.WriteLine($"  Sub {local}");
+        await Task.Delay(1000);
+        Console.WriteLine($"  Sub {local} done");
+    });
+}
+
+await runner.WhenAll();
+
+Console.WriteLine("bye");
